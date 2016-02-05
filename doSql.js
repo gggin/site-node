0bbafa1f02932c0abfb95436445349b8c77f2383
wangrui0810 <492847247@qq.com>
2016-02-05 15:20:31 +0800

    添加查询函数

--- doSql.js
+++ doSql.js
@@ -15,6 +15,7 @@ var  userAddSql = 'INSERT INTO user(id, email, pass, passwd, t, u, d, transfer_e
 var  userAddSql_Params = ['8', 'test@test.com', '123456', '0000000', '1410609560', '0', '0',
  '9320666234', '50000', '1', '1', '7', '0', '0'];
 
+var  userSelectSql = 'SELECT * FROM user';
 
 
 function doInsert_(x, y){
@@ -31,9 +32,26 @@ function doInsert_(x, y){
       });
 
   connection.end();
-
-
 }
 
+function doSelect_(x, callback){
+  connection.query(x,  function (err, result) {
+      if(err){
+      console.log('[INSERT ERROR] - ',err.message);
+      return;
+      }
+
+      console.log('--------------------------SELECT----------------------------');
+      callback(result);
+      console.log('--------------------------------------------------------\n\n');
+      });
+
+  connection.end();
+}
+/*
 doInsert_(userAddSql, userAddSql_Params);
+*/
+doSelect_(userSelectSql, function(x){
+        console.log(x);
+});
 
